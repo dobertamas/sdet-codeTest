31be8f99e9bd572a5cbc2ed731d18c7cdce149af
Brandon <brandon.williams@happyreturns.com>
2020-02-27 12:20:30 -0800

    Adding instructions

--- src/test/java/com/happyreturns/test/BaseClass.java
+++ src/test/java/com/happyreturns/test/BaseClass.java
@@ -4,7 +4,15 @@ public class BaseClass {
 
 
     /**
+     *  CHALLENGE :
+     *  For our challenge, we need to create a Selenium test that navigates to Google.com
+     *  and performs a search for "Happy Returns".
      *
+     *  After the results are displayed, assert that www.happyreturns.com is the first result
+     *  returned from Google.
      *
+     *  Time: 45 minutes
      */
+
+
 }
